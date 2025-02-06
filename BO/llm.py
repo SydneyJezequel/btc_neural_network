e5de482832c2230f7eeea5f39b9db90695442aa4
SydneyJezequel <107682562+SydneyJezequel@users.noreply.github.com>
2025-02-06 23:08:30 +0100

    Implémentation du llm - analyse des prédiction + du sentiment de marché

--- BO/llm.py
+++ BO/llm.py
@@ -20,11 +20,9 @@ class Llm:
 
     def analyze_predictions(self):
         """ Analyser les prédictions avec un LLM via une API """
-
         # Convertir les prédictions et les valeurs réelles en chaînes de caractères :
         predictions_str = ", ".join(map(str, self.all_predictions))
         true_values_str = ", ".join(map(str, self.all_true_values))
-
         # Créer le prompt pour l'analyse :
         analysis_prompt = f"""
         Voici les prédictions du modèle : {predictions_str}
@@ -37,19 +35,18 @@ class Llm:
 
     def call_llm(self, analysis_prompt):
         """ Appel du LLM via Api """
-
+        # Header de la requête :
         headers = {
             "Content-Type": "application/json",
             "Authorization": f"Bearer {self.api_key}"
         }
-
+        # Payload :
         data = {
             "prompt": analysis_prompt,
             "max_tokens": 150  # Ajustez selon vos besoins
         }
-
+        # Traitement de la requête :
         response = requests.post(self.api_url, headers=headers, json=data)
-
         if response.status_code == 200:
             analysis_results = response.json().get("choices", [{}])[0].get("text", "")
             # Afficher les résultats de l'analyse
@@ -58,3 +55,33 @@ class Llm:
         else:
             print(f"Erreur lors de l'appel à l'API LLM : {response.status_code}")
             print(response.text)
+
+
+
+    def search_news(self, query):
+        """ Recherche d'actualités """
+        url = "https://www.googleapis.com/customsearch/v1"
+        params = {
+            'key': self.api_key,
+            'cx': self.cse_id,
+            'q': query,
+            'num': 5  # Nombre de résultats à récupérer
+        }
+        response = requests.get(url, params=params)
+        return response.json()
+
+
+
+    def provide_market_feeling(self, query):
+        """ Traitement des résultats """
+        # Synthèse des actualités :
+        results = self.search_news(query)
+        for item in results.get('items', []):
+            print("Titre:", item['title'])
+            print("Lien:", item['link'])
+            print("Description:", item.get('snippet', 'Pas de description disponible'))
+            print("-" * 80)
+        # Analyse du sentiment de marché :
+        request = "Sur la base des résultats fournis, dis moi si le sentiment de marché est positif ou non et dis moi pourquoi. Résultats fournis : "
+        prompt = request + results
+        self.call_llm(prompt)
