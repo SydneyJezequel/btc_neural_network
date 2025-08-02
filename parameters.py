be4e54032cbc4f31ef3a3829218088bb89496225
SydneyJezequel <107682562+SydneyJezequel@users.noreply.github.com>
2025-08-02 23:16:36 +0200

    correction service de préparation des données (2)

--- parameters.py
+++ parameters.py
@@ -22,11 +22,11 @@ TIME_STEP = 15
 
 
 """ ************ LLM Parameters ************ """
-LLM_API_URL = '' # Cette ligne sera peut être à supprimer.
-LLM_API_KEY = '' # Cette ligne sera peut être à supprimer.
-FINANCIAL_NEWS_API_URL = 'A RECUPERER' # Cette ligne sera peut être à supprimer.
-FINANCIAL_NEWS_API_KEY = 'https://yfapi.net/v8/finance/some_endpoint' # Cette ligne sera peut être à supprimer.
-ANALYSIS_SENTIMENT_MODEL = 'cardiffnlp/twitter-roberta-base-sentiment-latest'  # Cette ligne sera peut être à supprimer.
+LLM_API_URL = ''
+LLM_API_KEY = ''
+FINANCIAL_NEWS_API_URL = 'To be provided'
+FINANCIAL_NEWS_API_KEY = 'https://yfapi.net/v8/finance/some_endpoint' #
+ANALYSIS_SENTIMENT_MODEL = 'cardiffnlp/twitter-roberta-base-sentiment-latest'
 
 
 
@@ -36,7 +36,7 @@ START_DATE = date(2014, 1, 1)
 END_DATE = date(2025, 1, 8)
 START_DATE  = START_DATE.isoformat()
 END_DATE = END_DATE.isoformat()
-API_TOKEN = "A RENSEIGNER"
+API_TOKEN = "To be provided"
 MARKET_NEWS_API_URL = f"https://eodhd.com/api/news?s=btc-usd.cc&from={START_DATE}&to={END_DATE}&api_token={API_TOKEN}&fmt=json"
 MARKET_SCORES_API_URL = f"https://eodhd.com/api/sentiments?s=btc-usd.cc&from={START_DATE}&to={END_DATE}&api_token={API_TOKEN}&fmt=json"
 
@@ -44,7 +44,7 @@ MARKET_SCORES_API_URL = f"https://eodhd.com/api/sentiments?s=btc-usd.cc&from={ST
 
 
 """ ************ LLM Parameters (API monster) ************ """
-API_MONSTER_KEY = 'A RENSEIGNER'
+API_MONSTER_KEY = 'To be provided'
 BASE_URL = "https://llm.monsterapi.ai/v1/"
 MODEL_NAME = 'monsterapi/Llama3.3_70b'
 
